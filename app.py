d2bafda87b07cd27d982f857af46879c9826682e
tran.tam <tam.tran@meosys.com>
2024-06-28 08:49:38 +0700

    202400628のソースコミット

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 
 UPLOAD_FOLDER = 'invoices'
-MODEL_BEST = 'spacy_v2\output\model-best'
+MODEL_BEST = 'spacy_v2/output/model-best'
 BASE_WORD = 'base_word.txt'
-API_KEY = '1234567890poi'
+API_KEY = '9ZHt2gprBdGYe0kbFL7eb40DEFPEqQ8G'
+RANGE = 0.8
 
 app = Flask(__name__)
 app.secret_key = "secret key"
@@ -12,3 +13,4 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 app.config['MODEL_BEST'] = MODEL_BEST
 app.config['BASE_WORD'] = BASE_WORD
 app.config['API_KEY'] = API_KEY
+app.config['RANGE'] = RANGE
--- server.py
+++ server.py
@@ -10,27 +10,20 @@ import re
 import uuid
 import spacy
 import pdfplumber
-from paddleocr import PaddleOCR,draw_ocr
-from flask import Flask, request, redirect, jsonify
+from paddleocr import PaddleOCR
+from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-import easyocr
+from gevent.pywsgi import WSGIServer
+from flask_cors import CORS, cross_origin
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+amt_re = re.compile(r'(\d+([,.]?\d)*)')
+cur_re = re.compile(r'[円￥半]')
 
 ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def recognize_text(img_path):
-    reader = easyocr.Reader(['ja'])
-    return reader.readtext(img_path, detail = 0, paragraph=True)
-
-def overlay_easyocr_text(img_path):
-	info = '' 
-	result = recognize_text(img_path)
-    # if OCR prob is over 0.5, overlay bounding box and text
-	for text in result:
-		info = info  + text + ' '
-	return info
 
 def overlay_paddleocr_text(img_path):
 	# Paddleocr supports Chinese, English, French, German, Korean and Japanese.
@@ -39,18 +32,53 @@ def overlay_paddleocr_text(img_path):
 	ocr = PaddleOCR(use_angle_cls=True, lang='japan', use_gpu=False) # need to run only once to download and load model into memory
 	info = ''
 	result = ocr.ocr(img_path, cls=True)
+	m_result = []
+	max_size_box = 0
+	max_size_value = 0
 	for idx in range(len(result)):
 		res = result[idx]
 		for line in res:
 			info = info  + line[1][0] + ' '
+			x1, _, x2, _ = line[0]
+			m_result.append([x2[1] - x1[1], line[1][0]])
+			if (x2[1] - x1[1])  >  max_size_box and amt_re.search(line[1][0]) and cur_re.search(line[1][0]):
+				max_size_value = line[1][0]
+				max_size_box = x2[1] - x1[1]
 
-	return info
+	m_result.sort(reverse=True)
+	return m_result, max_size_box, max_size_value, info
 
 def overlay_pdf_text(pdf_path):
+	info = ''
+	m_result = []
+	max_size_box = 0
+	max_size_value = 0
 	with pdfplumber.open(pdf_path) as pdf:
-		page = pdf.pages[0]
-		info = page.extract_text()
-		return info.replace("\n", "")
+		for page in pdf.pages:
+			text_elements = page.extract_words()
+			for element in text_elements:
+				m_result.append([element['height'], str(element['text'])])
+				info = info  + str(element['text']) + ' '
+				if element['height']  >  max_size_box and amt_re.search(element['text']) and cur_re.search(element['text']):
+					max_size_value = element['text']
+					max_size_box = element['height']
+	
+	m_result.sort(reverse=True)
+	return m_result, max_size_box, max_size_value, info
+
+def verify_result(m_result, max_size_box, max_size_value, result):
+	print(result)
+	print(max_size_value)
+	if str(result) in str(max_size_value):
+		return result
+	else:
+		for i in range(len(m_result)):
+			if ((str(result) in str(m_result[i][1])) and amt_re.search(m_result[i][1])):
+				if (m_result[i][0] / max_size_box > app.config['RANGE']):
+					return result
+				else: 
+					return amt_re.search(max_size_value).group(1)
+		return result
 		 
 def record_log(str):
 	with open('tmp/data.txt', 'r') as f:
@@ -75,6 +103,7 @@ def create_folder():
 	
 
 @app.route('/invoices', methods=['POST'])
+@cross_origin()
 def invoices():
 	param_key = request.args.get('API_KEY')
 	if param_key is None or param_key != app.config['API_KEY']:
@@ -109,26 +138,32 @@ def invoices():
 	# Load the model
 	ner_categories = ["MONEY"]
 	nlp = spacy.load(app.config['MODEL_BEST'])
+	m_result = []
+	max_size_box = 0
+	max_size_value = 0
 	directory_files = os.listdir(unique_folder_name)
 	for file in directory_files:
 		detected = False
 		invoice_file = os.path.join(unique_folder_name, file)
 		if file.rsplit('.', 1)[1].lower() == 'pdf':
-			info = overlay_pdf_text(invoice_file)
+			m_result, max_size_box, max_size_value, info = overlay_pdf_text(invoice_file)
+			if info == '':
+				m_result, max_size_box, max_size_value, info = overlay_paddleocr_text(invoice_file)
 		else:
-			info = overlay_paddleocr_text(invoice_file)
+			m_result, max_size_box, max_size_value, info = overlay_paddleocr_text(invoice_file)
 		doc = nlp(format_string(info))
-		amt_re = re.compile(r'(\d+([,.]?\d)*)')
-		with open (app.config['BASE_WORD'],'r') as fid:
+		
+		with open (app.config['BASE_WORD'],'r', encoding="shift_jis") as fid:
 			for line in fid:
 				for ent in doc.ents:
 					if ent.label_ in ner_categories:
 						token = ent.text.replace(' ', '')
 						word = line.replace("\n", "")
-						
 						if amt_re.search(token) and (word in token):
 							value = amt_re.search(token).group(1)
-							i = {'name': file.split('.')[0], 'value': value}
+							if (max_size_value != 0):
+								value = verify_result(m_result, max_size_box, max_size_value, value)
+							i = {'name': file.split('.')[0], 'value': value, 'currency': '円'}
 							data.append(i)
 							detected = True
 							break
@@ -146,4 +181,5 @@ def invoices():
 	resp = jsonify({'message' : 'Success', 'data': data})
 	resp.status_code = 201
 	return resp
-app.run(debug=False)
+if __name__ == "__main__":
+    app.run()
